eac7e97c19da98a97ac919a6683ae23144281553
qamiralii <a.bitpins@gmail.com>
2025-03-07 15:08:40 +0330

    first commit

---
+++ src/app/+navigations/private-nav/private-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-nav',
+  imports: [],
+  templateUrl: './private-nav.component.html',
+  styleUrl: './private-nav.component.scss'
+})
+export class PrivateNavComponent {
+
+}
---
+++ src/app/+navigations/public-nav/public-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-nav',
+  imports: [],
+  templateUrl: './public-nav.component.html',
+  styleUrl: './public-nav.component.scss'
+})
+export class PublicNavComponent {
+
+}
