3eaf6f9fe6fc499190da1c7f1f65483a330721a8
qamiralii <a.bitpins@gmail.com>
2025-03-07 17:43:21 +0330

    using angular material

--- src/app/components/angular-material-buttons/angular-material-buttons.component.ts
+++ src/app/components/angular-material-buttons/angular-material-buttons.component.ts
@@ -1,14 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-angular-material-buttons',
-//   imports: [],
-//   templateUrl: './angular-material-buttons.component.html',
-//   styleUrl: './angular-material-buttons.component.scss'
-// })
-// export class AngularMaterialButtonsComponent {
-
-// }
 import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
--- src/app/pages/public/login/ui/login.component.ts
+++ src/app/pages/public/login/ui/login.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonOverviewExample } from "../../../../components/angular-material-buttons/angular-material-buttons.component";
+import { InputClearableExample } from "../../../../components/angular-material-input/angular-material-input.component";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ButtonOverviewExample, InputClearableExample],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
