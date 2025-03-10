2d046a89892451528ee96d2d00e62cf983446b7d
qamiralii <a.bitpins@gmail.com>
2025-03-10 19:45:17 +0330

    tried to fix btn

--- src/app/components/angular-material-input/ui/angular-material-input.component.ts
+++ src/app/components/angular-material-input/ui/angular-material-input.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 /**
  * @title Input with a clear button
--- src/app/pages/public/login/ui/login.component.ts
+++ src/app/pages/public/login/ui/login.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { ButtonOverviewExample } from "../../../../components/angular-material-buttons/angular-material-buttons.component";
-import { InputClearableExample } from "../../../../components/angular-material-input/angular-material-input.component";
+import { Component, inject } from '@angular/core';
+import { ButtonOverviewExample } from "../../../../components/angular-material-buttons/ui/angular-material-buttons.component";
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { InputClearableExample as sibil } from "../../../../components/angular-material-input/ui/angular-material-input.component";
+import { AngularMaterialPasswordComponent } from "../../../../components/angular-material-password/ui/angular-material-password.component";
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonOverviewExample, InputClearableExample],
+  imports: [ButtonOverviewExample, MatFormFieldModule, MatInputModule, sibil, AngularMaterialPasswordComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+login() {
+  this.rout.navigateByUrl("/admin")
+}
+  rout=inject(Router);
+
 
 }
