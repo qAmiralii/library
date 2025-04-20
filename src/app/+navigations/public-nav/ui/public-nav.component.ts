831ca392b1d5161e79393d7194d69bd0604cfef5
qamiralii <a.bitpins@gmail.com>
2025-04-20 21:43:37 +0330

    tailwind get start

--- src/app/+navigations/public-nav/ui/public-nav.component.ts
+++ src/app/+navigations/public-nav/ui/public-nav.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TestComponent } from "../../../components/test/test.component";
-import { AddressFormComponent } from "../../../components/address-form/address-form.component";
-import { MatTableComponent } from "../../../components/mat-table/mat-table.component";
-import { MatTreeComponent } from "../../../components/mat-tree/mat-tree.component";
 
 @Component({
   selector: 'app-public-nav',
-  imports: [RouterOutlet, RouterLink, TestComponent, AddressFormComponent, MatTableComponent, MatTreeComponent,],
+  imports: [RouterOutlet, RouterLink, TestComponent,],
   templateUrl: './public-nav.component.html',
   styleUrl: './public-nav.component.scss'
 })
--- src/app/pages/public/login/ui/login.component.ts
+++ src/app/pages/public/login/ui/login.component.ts
@@ -1,14 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { ButtonOverviewExample } from "../../../../components/angular-material-buttons/ui/angular-material-buttons.component";
 import { Router, RouterLink } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { InputClearableExample as sibil } from "../../../../components/angular-material-input/ui/angular-material-input.component";
-import { AngularMaterialPasswordComponent } from "../../../../components/angular-material-password/ui/angular-material-password.component";
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonOverviewExample, MatFormFieldModule, MatInputModule, sibil, AngularMaterialPasswordComponent, RouterLink],
+  imports: [ MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
